51edde12433e71a1c34608956dbdf70fa0db6427
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-08 23:58:36 +0900

    [Level. 2] 올바른 괄호

--- Programmers/Lv. 2/12909. 올바른 괄호/올바른 괄호.py
+++ Programmers/Lv. 2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,6 +1,5 @@
-# 일단 통과하는 코드 (효율성 시간 초과)
+#1 통과하는 코드 (효율성 시간 초과)
 def solution(s):
-    answer = True
     lst = [i for i in s]
     a = ''
     tmp = 0
@@ -20,3 +19,28 @@ def solution(s):
         return True
     else :
         return False
+    
+#2 간단하게 적는다고 효율성이 올라가진 않는다. (효율성 시간 초과)
+def solution(s):
+    while '()' in s :
+        s = s.replace("()", "")
+    
+    if s == "" : return True
+    else : return False
+
+# 통과
+def solution(s):
+    tmp = 0
+    for i in s:
+        if tmp < 0:
+            return False
+    
+        if i == '(': 
+            tmp += 1
+        else:
+            if i == ')':
+                tmp -= 1
+            else:
+                tmp = tmp
+            
+    return tmp == 0
