ea7e2748ad27dd77720263d7ddcc8f2fa94f581e
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-15 01:33:20 +0900

    [Level. 1] 모의고사

--- Programmers/Lv. 1/42840. 모의고사/모의고사.py
+++ Programmers/Lv. 1/42840. 모의고사/모의고사.py
@@ -1,4 +1,4 @@
-# 사람들은 생각보다 더 줄여서 풀어버린다.
+# 사람들은 생각보다 더 줄여서 풀어버린다. 그래서 if문을 저렇게 썼다.
 # 한눈에 이해라도 잘되니 상관없나...모르겠다.
 def solution(answers):
     answer = []
@@ -8,18 +8,12 @@ def solution(answers):
     oneans, twoans, thrans = 0, 0, 0
     
     for i in range(len(answers)):
-        if answers[i] == one[i%5]:
-            oneans += 1
-        if answers[i] == two[i%8]:
-            twoans += 1
-        if answers[i] == thr[i%10]:
-            thrans += 1
+        if answers[i] == one[i%5]: oneans += 1
+        if answers[i] == two[i%8]: twoans += 1
+        if answers[i] == thr[i%10]: thrans += 1
 
     tmp = max(oneans, twoans, thrans)
-    if tmp == oneans:
-        answer.append(1)
-    if tmp == twoans:
-        answer.append(2)
-    if tmp == thrans:
-        answer.append(3)
+    if tmp == oneans: answer.append(1)
+    if tmp == twoans: answer.append(2)
+    if tmp == thrans: answer.append(3)
     return answer
