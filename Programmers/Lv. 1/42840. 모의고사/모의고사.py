b5fbab7232eff83642295a24d42a8a5506898616
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-15 00:41:04 +0900

    [Level. 1] 모의고사

---
+++ Programmers/Lv. 1/42840. 모의고사/모의고사.py
@@ -0,0 +1,25 @@
+# 사람들은 생각보다 더 줄여서 풀어버린다.
+# 한눈에 이해라도 잘되니 상관없나...모르겠다.
+def solution(answers):
+    answer = []
+    one = [1,2,3,4,5]
+    two = [2,1,2,3,2,4,2,5]
+    thr = [3,3,1,1,2,2,4,4,5,5]
+    oneans, twoans, thrans = 0, 0, 0
+    
+    for i in range(len(answers)):
+        if answers[i] == one[i%5]:
+            oneans += 1
+        if answers[i] == two[i%8]:
+            twoans += 1
+        if answers[i] == thr[i%10]:
+            thrans += 1
+
+    tmp = max(oneans, twoans, thrans)
+    if tmp == oneans:
+        answer.append(1)
+    if tmp == twoans:
+        answer.append(2)
+    if tmp == thrans:
+        answer.append(3)
+    return answer
