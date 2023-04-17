624cfb24157f154b313cd5a318b3eaf809f63d09
Hyoungtaek <79441147+hyoungteak@users.noreply.github.com>
2023-04-17 17:49:15 +0900

    [Level. 2] 타겟 넘버

--- Programmers/Lv. 2/DFS, BFS/42577. 전화번호 목록/타겟 넘버.py
+++ Programmers/Lv. 2/DFS, BFS/42577. 전화번호 목록/타겟 넘버.py
@@ -17,3 +17,22 @@ def solution(numbers, target):
             answer += 1
             
     return answer
+
+# DFS (깊이 우선 탐색)
+answer = 0
+
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
