b6ee6518dbab0aa0bc993c9c2ccbc43247e9bdbc
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-04-24 15:50:50 +0900

    [level 2] Title: 피보나치 수, Time: 468.68 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -5,4 +5,5 @@ def solution(n):
     for i in range(0, n+1):
         if i+1 == n: break
         lst[i+2] = lst[i] + lst[i+1]
-    return lst[-1]%1234567
+    # 1234567 나누고 나머지를 못봐서 5분 고민함ㅋㅋ
+    return lst[-1] % 1234567
