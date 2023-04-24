002a895fa18e8bd0a6bae51bb6ed1b9bccd6e0b8
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-04-24 14:51:03 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.86 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,12 +1,10 @@
 def solution(s):
-    ans = 0
-    cnt = 0
+    ans, cnt = 0, 0
     
     while (s != "1"):
         for i in s: 
-            if i == "0":
-                cnt += 1
-        s = "".join(bin(int(len(s.replace("0", ""))))).replace("0b", "")
+            if i == "0": cnt += 1
+        s = "".join(bin(int(len(s.replace("0", "")))))[2:]
         ans += 1
             
     return [ans, cnt]
