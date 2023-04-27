b1c080cf57d5cd10a8f51fd7f08b7be370cead76
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-04-27 15:28:17 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,13 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-            
+
         for (int i = 0; i < n; i++) {
-            int num = (arr1[i] | arr2[i]);
-            String str = Integer.toBinaryString(num);
-                        
-            while (str.length() != n) {
-                str = "0" + str;
-            }
-            
+            String str = Integer.toBinaryString(arr1[i] | arr2[i]);
+            while (str.length() != n) str = "0" + str;
             answer[i] = str.replace('0', ' ').replace('1', '#');
         }
+        
         return answer;
     }
 }
