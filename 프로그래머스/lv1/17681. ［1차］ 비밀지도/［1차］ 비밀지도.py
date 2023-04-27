029400b7165d45eb89e3d9d67deebcff686fffa3
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-04-27 15:33:32 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,7 +2,7 @@ def solution(n, arr1, arr2):
     answer = []
     
     for i in range(n):
-        num = bin(arr1[i] | arr2[i])[2:].zfill(n)
+        num = bin(arr1[i] | arr2[i])[2:].zfill(n) # rjust(n, "0")도 된다. zfill()보다 더 범용적(?)이다.
         num = num.replace('0', ' ').replace('1', '#')
         answer.append(num)
     
