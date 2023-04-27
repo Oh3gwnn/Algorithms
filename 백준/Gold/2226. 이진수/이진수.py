b2d4691f51878a0b077514cdef46daed49bdcc6b
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-04-27 13:33:29 +0900

    [Gold IV] Title: 이진수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2226. 이진수/이진수.py
@@ -0,0 +1,23 @@
+''' 시간초과
+n = int(input())
+bin_num = ['1']
+
+for i in range(0, n-1):
+    for i in range(0, len(bin_num)):
+        if bin_num[i] == '0' :
+            bin_num[i] = '10'
+        elif bin_num[i] == '1' :
+            bin_num[i] = '01'
+
+    bin_num = (list(''.join(bin_num)))
+
+bin_num = ''.join(bin_num)
+cnt = bin_num.count('01')
+print (cnt)
+'''
+
+n = int(input())
+dp = [0]
+for i in range(1, n):
+    dp.append(2**(i - 1) - dp[i - 1])
+print(dp[n - 1])
