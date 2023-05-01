78c2186ce1690be6bb6efad823c41794d735df82
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-01 10:41:50 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,11 @@
 def solution(a, b, c):
-    for i in range(3):
-        if (a != b) & (b != c) & (a != c):
-            return a+b+c
-        elif (a == b) & (b == c):
-            return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
-        else :
-            return (a+b+c)*(a**2+b**2+c**2)
+    answer = a+b+c
+    
+    # | 대신 or 사용 가능
+    if a == b or b == c or c == a :
+        answer = (a+b+c)*(a**2+b**2+c**2)
+    # a==b==c 이런 식으로 3개 인수 비교 가능
+    if (a == b == c): 
+        answer = (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3)
+    
+    return answer
