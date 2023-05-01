76ff6bb512567876b83ad1f4c8d13e9e55c105e1
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-01 16:36:03 +0900

    [unrated] Title: 배열 만들기 2, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,22 +1,12 @@
 def solution(l, r):
     answer = []
-    start, end = 0, 0
-    
-    tmp = l
-    while tmp > 1:  
-        tmp //= 10
-        start += 1
-    
-    tmp = r
-    while tmp > 1: 
-        tmp //= 10
-        end += 1
-                        
-    for i in range(start, 2**end):
-        tmp = 5*int(bin(i)[2:])
+    start, tmp = 1, 5
+        
+    while True :
+        if tmp > r: break
+        tmp = 5 * int(bin(start)[2:])
         if l <= tmp <= r:
             answer.append(tmp)
+        start += 1
     
-    if not answer : answer.append(-1)
-    
-    return answer
+    return [-1] if not answer else answer
