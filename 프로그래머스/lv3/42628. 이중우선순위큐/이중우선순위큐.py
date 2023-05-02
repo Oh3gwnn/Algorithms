941b15081ba24a772721e5552281557f61bb88f3
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-02 12:43:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,30 +1,20 @@
 import heapq
+
 def solution(operations):
     answer = []
-    heap = []   # 힙 생성
-
-    for data in operations:
-        # 연산이 "I"일 경우 공백 뒤의 숫자를 heap에 넣음
-        if data[0] == "I":
-            heapq.heappush(heap, int(data[2:]))
-        # 연산이 "D"일 경우
-        else:
-            if len(heap) == 0:
-                pass
-            # 공백 뒤가 "-"일 경우 최소힙을 제거
-            elif data[2] == "-":
-                heapq.heappop(heap)
-            # 공백 뒤가 아무것도 아닌 경우
-            else:
-                # 힙에서 가장 큰 수를 제외하고 다시 힙에 넣음
-                heap = heapq.nlargest(len(heap), heap)[1:]
-                heapq.heapify(heap)
-
-    if heap:
-        answer.append(max(heap))
-        answer.append(min(heap))
-    else:
-        answer.append(0)
-        answer.append(0)
     
-    return answer
+    for i in operations:
+        if i[0] == "I" : 
+            heapq.heappush(answer, int(i[2:]))
+            
+        else:
+            if len(answer) == 0: pass
+        
+            elif i[2] == "-":
+                heapq.heappop(answer)
+            else:
+                answer = heapq.nlargest(len(answer), answer)[1:]
+                heapq.heapify(answer)
+                    
+    if not answer : return [0, 0]
+    else : return [max(answer), min(answer)]
