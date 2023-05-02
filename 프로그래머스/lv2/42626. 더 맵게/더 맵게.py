382fa51c76bb0aacd7a518e070ef5fc86607a6bd
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-02 10:45:10 +0900

    [level 2] Title: 더 맵게, Time: 1607.97 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,6 +1,6 @@
 # 내가한거랑 로직 똑같은데 try except문 사용해서 진짜 깔끔하다.
-# 근데 효율성(시간초과) 면에서 통과가 되지 않는데 그래서 다들 어쩔 수 없이
-# heapq를 사용하는 것 같다.
+# 근데 효율성(시간초과) 면에서 통과가 되지 않는데  
+# 그래서 heapq를 사용하는 것 같다.
 '''
 def solution(scoville, k):
      mix_cnt = 0
@@ -14,22 +14,23 @@ def solution(scoville, k):
 
      return mix_cnt
 '''
-# 힙은 push, pop 작동 시 자동정렬 기능이 추가되어있다.
+# 힙은 2k+1 정렬이 들어간 맨 앞자리 최소 값 배열 같은 느낌이다.
 # sort의 정렬 비용을 감소시킨다.
+
+# 이해하기 위해서 다시 풀어본 코드
 import heapq
 
 def solution(scoville, k):
-    heap = []
-        
-    for num in scoville:
-        heapq.heappush(heap, num)
+    answer = 0
+    heapq.heapify(scoville)
     
-    mix_cnt = 0
-    while heap[0] < k:
-        try:
-            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
-        except IndexError:
-            return -1
-        mix_cnt += 1
+    while 1:
+        min_sco = heapq.heappop(scoville)
+        if min_sco >= k: break
+        if not scoville: return -1
+        sec_sco = heapq.heappop(scoville)
+        heapq.heappush(scoville, min_sco + sec_sco*2)
+        
+        answer += 1
 
-    return mix_cnt
+    return answer
