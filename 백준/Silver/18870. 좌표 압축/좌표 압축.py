471a1b5be0ec84adf3572cfe7d60ea309f0be3b7
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-03 11:31:08 +0900

    [Silver II] Title: 좌표 압축, Time: 1840 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,3 +1,28 @@
+''' 내 풀이인데 통과는 한다. 근데 왜 이리 어렵게 풀었을까
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+coordinate = list(map(int, input().split()))
+dim = []
+cnt = 0
+
+for i, d in enumerate(coordinate): dim.append([d, i])
+dim.sort()
+tmp = dim[0][0]
+
+for i in range(len(dim)):
+    if tmp == dim[i][0]: dim[i][0] = cnt
+    elif tmp < dim[i][0]:
+        cnt += 1
+        tmp = dim[i][0]
+        dim[i][0] = cnt
+        
+dim.sort(key=lambda x : x[1])
+
+print(" ".join(str(i[0]) for i in dim))
+'''
+
 import sys
 
 input = sys.stdin.readline
