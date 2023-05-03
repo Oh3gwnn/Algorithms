62b4989262e13a49211793f1ba1ae28c980dbd44
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-03 11:28:34 +0900

    [Silver II] Title: 좌표 압축, Time: 1896 ms, Memory: 154804 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,22 +1,11 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
-coordinate = list(map(int, input().split()))
-dim = []
-cnt = 0
+arr = list(map(int, input().split()))
 
-for i, d in enumerate(coordinate): dim.append([d, i])
-dim.sort()
-tmp = dim[0][0]
-
-for i in range(len(dim)):
-    if tmp == dim[i][0]: dim[i][0] = cnt
-    elif tmp < dim[i][0]:
-        cnt += 1
-        tmp = dim[i][0]
-        dim[i][0] = cnt
-        
-dim.sort(key=lambda x : x[1])
-
-print(" ".join(str(i[0]) for i in dim))
+arr2 = sorted(list(set(arr)))
+dic = {arr2[i] : i for i in range(len(arr2))}
+for i in arr:
+    print(dic[i], end = ' ')
