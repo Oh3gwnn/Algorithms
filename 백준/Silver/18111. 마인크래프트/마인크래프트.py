959733dac5bfb89ec98d065057d642122c650152
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-09 18:03:41 +0900

    [Silver II] Title: 마인크래프트, Time: 428 ms, Memory: 124336 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,6 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+# 1차원배열과 min, max로 범위 축소한 풀이 464ms(얘도 pypy3로 안하면 시간초과)
+import sys
+input = sys.stdin.readline
+
 n, m, b = map(int, input().split())
 lst = []
 for i in range(n): lst += list(map(int, input().split()))
@@ -25,3 +29,26 @@ for t in range(max(lst), min(lst)-1, -1):
             ans = t
 
 print(anstime, ans)
+
+# 브루스포스 풀이(시간초과 -> pypy3로 변경 -> 통과) 816ms
+# n, m, b = map(int, input().split())
+# lst = [list(map(int, input().split())) for _ in range(n)]
+# ans = sys.maxsize
+# idx = 0
+
+# for g in range(min(min(lst)), max(max(lst))+1):
+#     maxg, ming = 0, 0
+
+#     for i in range(n):
+#         for j in range(m):
+#             if lst[i][j] >= g:
+#                 maxg += lst[i][j] - g
+
+#             else: ming += g - lst[i][j]
+    
+#     if maxg + b >= ming:
+#         if ming + (maxg * 2) <= ans:
+#             ans = ming + (maxg * 2)
+#             idx = g
+
+# print(ans, idx)
