a78134cd1e52e31541a3ba9a29a279995a924b6d
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-18 10:45:02 +0900

    [level 2] Title: 올바른 괄호, Time: 20.64 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') st.push(s.charAt(i));
+            else if (s.charAt(i) == ')') {
+                if (st.isEmpty()) return false;
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
