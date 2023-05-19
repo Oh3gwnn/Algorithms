72ff9c1e389bd637716b9083e15b77b4170d7da3
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-20 00:14:29 +0900

    Update 나는야 포켓몬 마스터 이다솜.py

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,3 +1,6 @@
+# 왜 리스트, 딕셔너리 하나 이렇게하면 시간초과 뜨고
+# 딕셔너리 두개로 분리하면 시간초과가 안뜰까? 난잘몰
+
 import sys
 input = sys.stdin.readline
 
@@ -13,3 +16,14 @@ for _ in range(m):
     tmp = input().rstrip()
     if tmp.isdigit(): print(pk_num[int(tmp)])
     else: print(pk_name[tmp])
+
+# import sys
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# pk_list = [str(input().rstrip()) for _ in range(n)]
+
+# for _ in range(m):
+#     tmp = input().rstrip()
+#     if tmp.isdigit(): print(pk_list[int(tmp)])
+#     else: print(pk_list.index(tmp)+1)
