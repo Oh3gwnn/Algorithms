22d9f62a84327b133b0e56b494ea339e2c94aff1
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-05-22 00:09:33 +0900

    [Gold V] Title: 적록색약, Time: 84 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -11,18 +11,15 @@ dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 res1, res2 = 0, 0
 que = deque()
 
-def bfs(x, y):
-    que.append([x, y])
-    visited[x][y] = 1
-
+def bfs():
     while que:
         x, y = que.popleft()
+        visited[x][y] = 1
         
         for i in range(4):
             kx = x + dx[i]
             ky = y + dy[i]
-
-            if 0 <= kx < n and 0 <= ky < n and visited[kx][ky] == 0:
+            if (0 <= kx < n) and (0 <= ky < n) and visited[kx][ky] == 0:
                 if lst[x][y] == lst[kx][ky]:
                     visited[kx][ky] = 1
                     que.append([kx, ky])
@@ -30,7 +27,8 @@ def bfs(x, y):
 for i in range(n):
     for j in range(n):
         if visited[i][j] == 0:
-            bfs(i, j)
+            que.append([i, j])
+            bfs()
             res1 += 1
 
 visited = [[0] * n for _ in range(n)]
@@ -42,7 +40,8 @@ for i in range(n):
 for i in range(n):
     for j in range(n):
         if visited[i][j] == 0:
-            bfs(i, j)
+            que.append([i, j])
+            bfs()
             res2 += 1
 
 print(res1, res2)
