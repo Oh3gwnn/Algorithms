4828d864f9721ce89b2e0bc9f3d9dec19a80806b
Hyoungtaek <79441147+Oh3gwnn@users.noreply.github.com>
2023-06-06 22:45:31 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,12 +3,11 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
+S = input()
 
 s = "I" + "OI" * n
+
 cnt = 0
-
-S = str(input())
-
 for i in range(m):
     if s == S[i:i+len(s)]:
         cnt += 1
